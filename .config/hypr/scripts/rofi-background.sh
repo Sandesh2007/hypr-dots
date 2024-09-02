9297bd618794939cdb9387b046e3cc0a10a2a9ff
SAN_DESH63 <sandeshsharma924@gmail.com>
2024-09-02 13:46:16 +0545

    updated

--- .config/hypr/scripts/rofi-background.sh
+++ .config/hypr/scripts/rofi-background.sh
@@ -1,1 +1,3 @@
-swww query | grep -oP '(?<=image: ).*' | xargs -I {} cp "{}" ~/.cache/current_wallpaper.png && swww query | grep -oP '(?<=image: ).*' | xargs -I {} cp "{}" /home/sandesh/.mozilla/firefox/nst4cois.default-release/chrome/ShyFox/content/current_wallpaper.png
+swww query | grep -oP '(?<=image: ).*' | xargs -I {} cp "{}" ~/.cache/current_wallpaper.png 
+#swww query | grep -oP '(?<=image: ).*' | xargs -I {} cp "{}" /home/sandesh/.mozilla/firefox/nst4cois.default-release/chrome/ShyFox/content/#current_wallpaper.png
+# uncomment the line if you are using the shhyfox and want background as same as your wallpaper 
